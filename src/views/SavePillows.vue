cb08678f0b332c97de2f321bd2d6794c8e95b6f5
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 20:57:00 +0100

    template html for page count save

--- src/views/SavePillows.vue
+++ src/views/SavePillows.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Hello World</h1>
+    <div>
+        <div>
+            <form>
+                <label>Email</label>
+                <input>
+                <label>Date of birth</label>
+                <input>
+                <label>Checkbox</label>
+                <input>
+            </form>
+            <button>Save choice</button>
+        </div>
+    </div>
 </template>
