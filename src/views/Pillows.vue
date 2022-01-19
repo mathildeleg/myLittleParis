59e157a7c5135263d6ad65fb503e472905ecc120
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 22:21:31 +0100

    use mutation decrement in pillows vue

--- src/views/Pillows.vue
+++ src/views/Pillows.vue
@@ -2,9 +2,9 @@
     <div>
         <img src="../assets/coussins/1.png" />
         <div id="count">
-            <button>-</button>
+            <button v-if="isMinCount()" v-on:click="decrement">-</button>
             <div>{{pillowCount}}</div>
-            <button  v-if="isMaxCount()" v-on:click="increment">+</button>
+            <button v-if="isMaxCount()" v-on:click="increment">+</button>
         </div>
         <router-link to="/pillows/save">Next Page</router-link>
     </div>
@@ -13,7 +13,7 @@
 <script lang="ts">
   import {Component, Prop, Vue} from 'vue-property-decorator';
   import {Action, Getter} from 'vuex-class';
-  import { MAX_PILLOWS } from '../store/pillows/mutations';
+  import { MAX_PILLOWS, MIN_PILLOWS } from '../store/pillows/mutations';
 
   const namespace: string = 'pillows';
 
@@ -29,8 +29,16 @@
         this.$store.commit('pillows/increment');
     }
 
+    decrement() {
+        this.$store.commit('pillows/decrement');
+    }
+
     isMaxCount() {
         return this.pillowCount < MAX_PILLOWS;
+    }
+
+    isMinCount() {
+        return this.pillowCount > MIN_PILLOWS;
     }
 
   }
