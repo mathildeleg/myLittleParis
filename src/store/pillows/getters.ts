bef03bfcb2daae867276fab12dd502931131051c
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 21:18:44 +0100

    create getter for the count

--- src/store/pillows/getters.ts
+++ src/store/pillows/getters.ts
@@ -3,5 +3,7 @@ import { PillowsState } from './types';
 import { RootState } from '../types';
 
 export const getters: GetterTree<PillowsState, RootState> = {
-  
+  pillowCount(state): number{
+    return state.pillowCount;
+  }
 };
