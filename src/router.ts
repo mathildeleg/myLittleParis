383ddbfcb6d4743308c68a9e9813a54b6f070450
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 20:42:07 +0100

    create first page for pillow count

--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
+// import Home from './views/Home.vue';
+// import About from './views/users.vue';
+import Pillows from './views/Pillows.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home,
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: About,
+    // },
     {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
+      path: '/pillows',
+      name: 'pillows',
+      component: Pillows,
     },
   ],
 });
