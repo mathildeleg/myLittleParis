616299c60aae48b1f5621c6418a7f09ce0d0d7e5
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 21:53:11 +0100

    use mutation increment in pillows vue

--- src/views/Pillows.vue
+++ src/views/Pillows.vue
@@ -4,7 +4,7 @@
         <div id="count">
             <button>-</button>
             <div>{{pillowCount}}</div>
-            <button>+</button>
+            <button  v-if="isMaxCount()" v-on:click="increment">+</button>
         </div>
         <router-link to="/pillows/save">Next Page</router-link>
     </div>
@@ -13,6 +13,7 @@
 <script lang="ts">
   import {Component, Prop, Vue} from 'vue-property-decorator';
   import {Action, Getter} from 'vuex-class';
+  import { MAX_PILLOWS } from '../store/pillows/mutations';
 
   const namespace: string = 'pillows';
 
@@ -20,9 +21,16 @@
   export default class Pillows extends Vue {
     @Getter('pillowCount', {namespace}) pillowCount: number;
 
-
     constructor() {
       super();
+    }
+
+    increment() {
+        this.$store.commit('pillows/increment');
+    }
+
+    isMaxCount() {
+        return this.pillowCount < MAX_PILLOWS;
     }
 
   }
