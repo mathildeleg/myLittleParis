5850290b77ecf74a2976c885fafafbf7b1b4f21b
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 21:52:54 +0100

    create mutation increment

--- src/store/pillows/mutations.ts
+++ src/store/pillows/mutations.ts
@@ -1,6 +1,12 @@
 import { MutationTree } from 'vuex';
 import { PillowsState } from './types';
 
-export const mutations: MutationTree<PillowsState> = {
+export const MAX_PILLOWS = 10;
 
+export const mutations: MutationTree<PillowsState> = {
+  increment(state) {
+    if(!(state.pillowCount === MAX_PILLOWS)) {
+      state.pillowCount += 1;
+    }
+  },
 };
