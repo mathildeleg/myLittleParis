f4c20663756ae2825d984a10e11151f30b1b7582
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 21:18:54 +0100

    use getter for the count on pillow page

--- src/views/Pillows.vue
+++ src/views/Pillows.vue
@@ -3,9 +3,27 @@
         <img src="../assets/coussins/1.png" />
         <div id="count">
             <button>-</button>
-            <div>number</div>
+            <div>{{pillowCount}}</div>
             <button>+</button>
         </div>
         <router-link to="/pillows/save">Next Page</router-link>
     </div>
 </template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator';
+  import {Action, Getter} from 'vuex-class';
+
+  const namespace: string = 'pillows';
+
+  @Component
+  export default class Pillows extends Vue {
+    @Getter('pillowCount', {namespace}) pillowCount: number;
+
+
+    constructor() {
+      super();
+    }
+
+  }
+</script>
