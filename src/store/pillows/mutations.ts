b8258a225b0d823b6deb81d11ae1e39afe60aa43
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-19 22:21:19 +0100

    create mutation decrement

--- src/store/pillows/mutations.ts
+++ src/store/pillows/mutations.ts
@@ -2,11 +2,17 @@ import { MutationTree } from 'vuex';
 import { PillowsState } from './types';
 
 export const MAX_PILLOWS = 10;
+export const MIN_PILLOWS = 0;
 
 export const mutations: MutationTree<PillowsState> = {
   increment(state) {
     if(!(state.pillowCount === MAX_PILLOWS)) {
       state.pillowCount += 1;
+    }
+  },
+  decrement(state) {
+    if(!(state.pillowCount === MIN_PILLOWS)) {
+      state.pillowCount -= 1;
     }
   },
 };
