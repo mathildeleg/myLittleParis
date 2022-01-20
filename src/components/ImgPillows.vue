a1893989a8af0e0a8ef7578938e0ed60af987d85
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-20 12:23:29 +0100

    create component img pillows

---
+++ src/components/ImgPillows.vue
@@ -0,0 +1,25 @@
+<template>
+    <img :src="getImgUrl()" />
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator';
+
+  @Component
+  export default class ImgPillows extends Vue {
+    @Prop([Number]) readonly pillowCount: number
+    
+    constructor() {
+      super();
+    }
+
+    getImgUrl() {
+        try {
+            return require(`../assets/coussins/${this.pillowCount}.png`);
+        } catch (error) {
+            return null;
+        }
+    }
+
+  }
+</script>
