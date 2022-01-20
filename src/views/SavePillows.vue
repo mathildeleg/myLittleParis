cbd67ddef072b4380da2eaf4d17d5fb69956de64
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-20 08:46:26 +0100

    improve html form

--- src/views/SavePillows.vue
+++ src/views/SavePillows.vue
@@ -2,12 +2,12 @@
     <div>
         <div>
             <form>
-                <label>Email</label>
-                <input>
-                <label>Date of birth</label>
-                <input>
-                <label>Checkbox</label>
-                <input>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+                <label for="birthdate">Date of birth</label>
+                <input type="date" name="birthdate" id="birthdate">
+                <label for="optin">I agree to...</label>
+                <input type="checkbox" name="optin" id="optin">
             </form>
             <button>Save choice</button>
         </div>
