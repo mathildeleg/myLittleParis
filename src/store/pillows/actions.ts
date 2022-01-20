b4c80c1faf82f767eca48fd3182f95690577aab9
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-20 11:06:24 +0100

    create postForm

--- src/store/pillows/actions.ts
+++ src/store/pillows/actions.ts
@@ -5,5 +5,28 @@ import { RootState } from '../types';
 
 
 export const actions: ActionTree<PillowsState, RootState> = {
-
+    async postForm(context): Promise<any> {
+        if(context.state.subscriber == null){
+            return
+        }
+        const { email, birthDate, optin } = context.state.subscriber;
+        const { pillowCount } = context.state;
+        try {
+            const response = await fetch('https://61bb0826e943920017784bb1.mockapi.io/api/v1/suscribers', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    birthDate,
+                    optin,
+                    choice: pillowCount,
+                })
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 };
