022401bcd5d6f83de89fbd61434899ed0035c0c3
Mathilde Legrand <m.legrand.duchaxel@gmail.com>
2022-01-20 11:01:46 +0100

    add subscriber info to initial state

--- src/store/pillows/index.ts
+++ src/store/pillows/index.ts
@@ -7,6 +7,7 @@ import { RootState } from '../types';
 
 export const state: PillowsState = {
   pillowCount: 0,
+  subscriber: {},
 };
 
 const namespaced: boolean = true;
--- src/store/pillows/types.ts
+++ src/store/pillows/types.ts
@@ -1,3 +1,10 @@
 export interface PillowsState {
   pillowCount: number;
+  subscriber: Subscriber;
+}
+
+export interface Subscriber {
+  email?: string;
+  birthDate?: Date;
+  optin?: boolean;
 }
